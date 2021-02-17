905843588428264cb7bba7b5d1a76d1450e7d4bc
Oscar Farre <oscar.farre.andres@gmail.com>
2021-02-17 12:16:57 +0100

    soy un trol

--- src/components/AnimalAvatar.vue
+++ src/components/AnimalAvatar.vue
@@ -5,7 +5,7 @@
         src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y"
       />
     </ion-avatar>
-    <ion-label>Chip Avatar</ion-label>
+    <ion-label>Chip Ava   tar</ion-label>
   </ion-chip>
   <h2>hola!</h2>
 </template>
