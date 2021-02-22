d4e9ccdd1711603eccf66521c68365058c4cf899
marc <mbellydo@gmail.com>
2021-02-22 11:49:25 +0100

    btn delete + fixes

--- src/components/AnimalForm.vue
+++ src/components/AnimalForm.vue
@@ -2,29 +2,50 @@
   <form action="" class="ion-padding" @submit.prevent="addAnimal">
     <ion-list>
       <!-- Image item -->
-      <ion-item>
-        <div id="image">
+      <ion-item v-if="imagesList.length == 0">
+        <div class="image">
+          <!-- <ion-icon :icon="imagesOutline" size="large" @click="takePicture"></ion-icon> -->
+          <img src="../../public/assets/images-outline.svg" alt="svg add image" width="100" height="100" @click="takePicture">
+        </div>
+      </ion-item>
+      <ion-item v-else>
+        <div class="image">
           <ion-grid>
             <ion-row>
               <ion-col v-for="index in 5" :key="index">
-                <ion-icon :icon="images" size="large" @click="takePicture" v-if="index > imagesList.length"></ion-icon>
+                <ion-icon
+                  :icon="imagesOutline"
+                  size="large"
+                  @click="takePicture"
+                  v-if="index > imagesList.length"
+                ></ion-icon>
                 <div class="preview" v-else>
-                  <img :src="imagesList[index-1].preview" @click="setOpen(true)"/>
-                  <ion-modal :is-open="isOpen" @onDidDismiss="setOpen(false)" css-class="image-modal">
-                    <div>
-                      <img :src="imagesList[index-1].preview" />
-                      <ion-fab vertical="bottom" horizontal="center">
-                        <ion-fab-button color="danger">
-                          <ion-icon :icon="trash" size="large"></ion-icon>
-                          Delete
-                        </ion-fab-button>
-                      </ion-fab>
-                    </div>
-                  </ion-modal>
+                  <img
+                    :src="imagesList[index - 1].preview"
+                    @click="setOpen(index)"
+                  />
                 </div>
               </ion-col>
             </ion-row>
           </ion-grid>
+          <ion-modal
+            :is-open="isOpen"
+            @onDidDismiss="setClose"
+            css-class="image-modal"
+          >
+            <div>
+              <img :src="imageToPreview" />
+              <ion-fab vertical="bottom" horizontal="center">
+                <ion-fab-button color="danger">
+                  <ion-icon
+                    :icon="trash"
+                    size="large"
+                    @click="deleteImage()"
+                  ></ion-icon>
+                </ion-fab-button>
+              </ion-fab>
+            </div>
+          </ion-modal>
         </div>
         <p v-if="error">{{ error }}</p>
       </ion-item>
@@ -42,11 +63,7 @@
       <!-- Sex item -->
       <ion-item>
         <ion-label>Sexo</ion-label>
-        <ion-select
-          v-model="sex"
-          okText="Aceptar"
-          cancelText="Cancelar"
-        >
+        <ion-select v-model="sex" okText="Aceptar" cancelText="Cancelar">
           <ion-select-option value="f">Femenino</ion-select-option>
           <ion-select-option value="m">Masculino</ion-select-option>
         </ion-select>
@@ -54,11 +71,7 @@
       <!-- Species Item -->
       <ion-item>
         <ion-label>Especie</ion-label>
-        <ion-select
-          v-model="species"
-          okText="Aceptar"
-          cancelText="Cancelar"
-        >
+        <ion-select v-model="species" okText="Aceptar" cancelText="Cancelar">
           <ion-select-option value="0">Gato</ion-select-option>
           <ion-select-option value="1">Perro</ion-select-option>
           <ion-select-option value="2">Tortuga</ion-select-option>
@@ -136,11 +149,6 @@
           <ion-select-option value="zaragoza">Zaragoza</ion-select-option>
         </ion-select>
       </ion-item>
-      <!-- Image item -->
-      <ion-item>
-        <ion-label position="floating">Subir Imagen</ion-label>
-        <ion-input v-model="image" type="url"></ion-input>
-      </ion-item>
       <!-- Description Item -->
       <ion-item>
         <ion-label position="floating">Descripción</ion-label>
@@ -177,10 +185,18 @@ import {
   IonFab,
   IonFabButton,
 } from "@ionic/vue";
-import { images, trash } from "ionicons/icons";
+import { images, imagesOutline, trash } from "ionicons/icons";
 import { Plugins, CameraResultType } from "@capacitor/core";
 
 const { Camera } = Plugins;
+
+/*function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}*/
 
 export default {
   components: {
@@ -212,10 +228,13 @@ export default {
       image: undefined,
       description: undefined,
       images: images,
+      imagesOutline: imagesOutline,
       trash: trash,
       imagesList: [],
       error: null,
       isOpen: false,
+      imageToPreview: undefined,
+      imageToDelete: undefined,
     };
   },
   methods: {
@@ -239,7 +258,7 @@ export default {
         },
         body: JSON.stringify(newAnimal),
       };
-
+      //this.uploadPhotos()
       fetch(url, postOptions)
         .then((response) => {
           if (response.ok) {
@@ -277,18 +296,55 @@ export default {
         this.error = "You can only upload 5 pictures!";
       }
     },
-    setOpen(isOpen){
-      this.isOpen = isOpen 
-    }
+    setOpen(index) {
+      this.isOpen = true;
+      this.imageToPreview = this.imagesList[index - 1].preview;
+      this.imageToDelete = index - 1 
+    },
+    setClose() {
+      this.isOpen = false;
+    },
+    deleteImage() {
+      this.imagesList.splice(this.imageToDelete, 1);
+      this.isOpen = false;
+    },
+    /*async uploadPhotos() {
+      for (let i = 0; i < this.imagesList.length; i++) {
+        await this.uploadPhoto(i);
+      }
+    },
+    async uploadPhoto(index) {
+      const user = auth.currentUser;
+      const guid = uuidv4();
+      const filePath = `${user?.uid}/images/${guid}.${this.imagesList[index].format}`;
+      const storageRef = storage.ref();
+
+      await storageRef
+        .child(filePath)
+        .putString(this.imagesList[index].base64String, "base64");
+      const url = await storageRef.child(filePath).getDownloadURL();
+
+      await db.collection("users").doc(user?.uid).collection("images").add({
+        image: url,
+      });
+    },*/
   },
 };
 </script>
 
 <style scoped>
-ion-label{
+ion-label {
   font-weight: 700;
 }
-div#image{
+div.image {
   margin: 0 auto;
+}
+ion-col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+ion-col img {
+  border-radius: 10px;
 }
 </style>
