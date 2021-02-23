0d05804712bbfb6d9a3b98262ec9f49046133d0f
DLS <dielasu@gmail.com>
2021-02-23 12:26:00 +0100

    Component added

--- src/views/AdminAnimals.vue
+++ src/views/AdminAnimals.vue
@@ -1,66 +1,29 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-item>
-        <ion-avatar slot="start">
-          <img src="/assets/avatar.png" />
-        </ion-avatar>
-        <ion-label>
-          <h2>Tu nombre</h2>
-          <p>Ver mi perfil</p>
-        </ion-label>
-        <ion-button>
-          <ion-icon :icon="chevronForwardOutline" />
-        </ion-button>
-      </ion-item>
-      <ion-button type="submit" expand="block" fill="solid"
-        >Ajustes búsqueda</ion-button
-      >
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <h1>Tus animales</h1>
-      <ion-button>
-        <ion-icon :icon="chevronForwardOutline" />
-      </ion-button>
-    </ion-content>
-    <ion-button>
-      <ion-icon :icon="addCircleOutline" />
-    </ion-button>
-  </ion-page>
+<ion-page>
+  <ion-content :fullscreen="true">
+    <admin-animals></admin-animals>
+  </ion-content>
+</ion-page>
 </template>
 
 <script>
-import { chevronForwardOutline, addCircleOutline } from "ionicons/icons";
-import {
-  IonPage,
-  IonHeader,
-  IonIcon,
-  IonButton,
-  IonAvatar,
-  IonItem,
-  IonLabel,
+ import AdminAnimals from '../components/AdminAnimals.vue'
+ import {
   IonContent,
+  IonPage,
 } from "@ionic/vue";
+
 export default {
-  data() {
-    return {
-      chevronForwardOutline,
-      addCircleOutline,
-    };
-  },
-  name: "admin-animals",
-  components: {
-    IonPage,
-    IonHeader,
+
+  components:{
+    AdminAnimals,
     IonContent,
-    IonIcon,
-    IonButton,
-    IonAvatar,
-    IonItem,
-    IonLabel,
-  },
-};
+    IonPage
+  }
+
+}
 </script>
 
-<style scoped>
+<style>
+
 </style>
